9c97172b48bbb903ff7350708ceaaf4139ab9ad2
Shivdev Panchal <shivdev.panchal@cygnetinfotech.com>
2020-09-18 13:12:38 +0530

    Seprate Models Project Reference Addition

---
+++ HarbourControlModels/BoatModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarbourControlModels
+{
+    public class BoatModel
+    {
+        public string Type { get; set; }
+        public int Speed { get; set; }
+        public int Perimeter { get; set; }
+        public DateTime JourneyStartTime { get; set; }
+        public DateTime JourneyEndTime { get; set; }
+    }
+}
